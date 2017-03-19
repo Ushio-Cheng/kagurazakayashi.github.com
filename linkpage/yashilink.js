a56b954c528eaeb181427fdbf8b64e114f15fd90
神楽坂雅詩 <cyscxchope@gmail.com>
2017-03-19 20:46:29 +0800

    [友链]生成友链网站UI对象

--- linkpage/yashilink.js
+++ linkpage/yashilink.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
             datdatacount = items.length;
             console.log("数据 "+datdatacount+": "+xhr.status+": "+xhr.statusText);
             console.log(responseTxt);
-            $("#d2").html(responseTxt);
             loaddata(items);
             $('body,html').scrollTop = 0;
         }
@@ -18,6 +17,34 @@ $(document).ready(function(){
 });
 function loaddata(data) {
     var contents = $("#contents");
+    var chtml = "";
+    $.each(data, function(i, items) {
+        chtml = chtml+'<div id="webview'+i+'" class="webview" style="background-image:url(linkpage/sitepic/'+items[4]+'.jpeg)"><div id="urlinfo'+i+'" class="urlinfo" style="opacity:0">'+items[2]+'</div><div class="infobtnbg"><div id="infobtn'+i+'" class="infobtn"><a href="'+items[3]+'">ⓘ</a></div></div><div class="webinfo"><h1>'+items[0]+'</h1><h2>'+items[1]+'</h2><h3>'+items[5]+'</h3></div></div>';
+    });
+    $("#contents").html(chtml);
+    $.each(data, function(i, items) {
+        var webview = $("#webview"+i);
+        webview.click(function() {
+            window.location = items[2];
+        });
+        webview.hover(
+            function () {
+                $("#urlinfo"+i).animate({
+                    "opacity":1
+                });
+            },
+            function () {
+                $("#urlinfo"+i).animate({
+                    "opacity":0
+                });
+            }
+        );
+        var infobtn = $("#infobtn"+i);
+        infobtn.click(function() {
+            window.location = items[3];
+        });
+    });
+    resize(true);
 }
 function resize(ani) {
 }
