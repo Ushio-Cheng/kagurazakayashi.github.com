b270c208bee0ce8ca1ec1657e1e5eaa3dde791ce
神楽坂雅詩 <cyscxchope@gmail.com>
2017-03-19 19:33:03 +0800

    [友链]从接口获取JSON数据

--- linkpage/yashilink.js
+++ linkpage/yashilink.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-
+    $.getJSON("linkpage/yashilink.json",function(responseTxt,statusTxt,xhr,data){
+        $(".spinner").remove();
+        if(statusTxt == "success") {
+            var items = xhr.responseJSON;
+            datdatacount = items.length;
+            console.log("数据 "+datdatacount+": "+xhr.status+": "+xhr.statusText);
+            console.log(responseTxt);
+            $("#d2").html(responseTxt);
+            loaddata(items);
+            $('body,html').scrollTop = 0;
+        }
+        if(statusTxt == "error") {
+            $("#contents").html("<center><h1>数据加载失败，请稍后刷新再试。</h1></center>");
+            console.error("数据加载失败: "+xhr.status+": "+xhr.statusText);
+        }
+    });
 });
+function loaddata(data) {
+    var contents = $("#contents");
+}
 function resize(ani) {
 }
 $(window).scroll(function() {
