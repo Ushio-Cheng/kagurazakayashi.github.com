87dad78d41c1c49855621a266b90228fb9948a46
神楽坂雅詩 <cyscxchope@gmail.com>
2017-04-04 23:09:12 +0800

    [关于]创建栏目列表

--- aboutpage/about.js
+++ aboutpage/about.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
     $('#dowebok').fullpage({
-        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90']
+        sectionsColor: [
+            '#1bbc9b',
+            '#4BBFC3',
+            '#7BAABE',
+            '#f90',
+            '#1bbc9b',
+            '#4BBFC3',
+            '#7BAABE',
+            '#f90',
+            '#1bbc9b',
+            '#4BBFC3'
+        ],
+        navigationTooltips: [
+            '生活(博客)',
+            '梦想(作品)',
+            '感恩(开源)',
+            '欢乐(游戏)',
+            '二维(动漫)',
+            '绮丽(换装)',
+            '友情(友链)',
+            '神楽(来源)',
+            '个性(性格)',
+            '神楽坂雅詩'
+        ],
+        navigation: true,
+        navigationColor: "#FE99CC",
+        scrollingSpeed: 700,
+        afterLoad: function(anchorLink,index) {
+            //console.log("afterLoad=",index,", anchorLink=",anchorLink);
+            var h1 = $("#section"+index+" h1");
+            var h2 = $("#section"+index+" h2");
+            h1.fadeIn(1000,function() {
+                h2.fadeIn(1000,null);
+            });
+        },
+        onLeave: function(index,direction) {
+            //console.log("onLeave=",index,", direction=",direction);
+            var h1 = $("#section"+index+" h1");
+            var h2 = $("#section"+index+" h2");
+            h1.css("display","none");
+            h2.css("display","none");
+        }
     });
 });
