389d9ccf7bad13022cf099ef7e61ba3a35ae479a
神楽坂雅詩 <cyscxchope@gmail.com>
2017-04-20 15:10:42 +0800

    [文章页]优化右下角弹出菜单按钮体验

--- nyatest/nya.js
+++ nyatest/nya.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-    var rightbottommenubtns = $("#rightbottommenubox2 li");
+    var rightbottommenubox = $("#rightbottommenubox");
+    var rightbottommenubtns = $("#rightbottommenuboxf li");
     $("#rightbottommenuswitch").click(function(){
-        if ($(this).text() == "+") {
-            $(this).text("-");
+        if (rightbottommenubox.attr("value") == "0") {
+            rightbottommenubox.attr("value","1");
+            $(this).stop();
+            var animation = "rightbottommenuopen 0.4s forwards";
+            $(this).css({"animation":animation,"-webkit-animation":animation,"-moz-animation":animation,"-o-animation":animation,"-ms-animation":animation});
             $.each(rightbottommenubtns, function(i, item) {
                 var speed = 200 * (rightbottommenubtns.length - i);
                 $(this).stop();
                 $(this).fadeIn(speed,null);
             });
         } else {
-            $(this).text("+");
+            rightbottommenubox.attr("value","0");
+            $(this).stop();
+            var animation = "rightbottommenuclose 0.3s forwards";
+            $(this).css({"animation":animation,"-webkit-animation":animation,"-moz-animation":animation,"-o-animation":animation,"-ms-animation":animation});
             $.each(rightbottommenubtns, function(i, item) {
                 var speed = 150 * i;
                 $(this).stop();
